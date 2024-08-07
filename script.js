e8cafac5213af548ee165848fbf14ea195d54528
sandraelsa16 <sandraelsavarughese@gmail.com>
2024-08-07 18:35:05 +0530

    files:added

---
+++ script.js
@@ -0,0 +1,16 @@
+const login = document.getElementById("login")
+const register = document.getElementById("register")
+const getStarted = document.getElementById("getstarted")
+
+login.addEventListener("click",function(){
+    window.location ='./login.html'
+})
+
+getStarted.addEventListener("click",function(){
+    window.location ='./login.html'
+})
+
+register.addEventListener("click",function(){
+    window.location='./register.html'
+})
+
